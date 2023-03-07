8b2f4229a4abf4140e139b2c896c1b38f80fd6f1
ryhhill1998 <ryhhill1998@outlook.com>
2023-03-07 10:50:07 +0000

    Added navbar component to blog

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import Navbar from "../common/components/Navbar/Navbar";
+
+const App = () => {
+    return (
+        <div className="App">
+            <Navbar />
+            <div className="content">
+                <h2>App Content</h2>
+            </div>
+        </div>
+    );
+}
+
+export default App;
