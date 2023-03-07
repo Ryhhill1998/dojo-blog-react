dc3315c579114557de2308dc012c53d71696cbc6
ryhhill1998 <ryhhill1998@outlook.com>
2023-03-07 18:57:37 +0000

    Added conditional loading message

--- src/common/components/Home/Home.jsx
+++ src/common/components/Home/Home.jsx
@@ -7,27 +7,33 @@ import PostsList from "../PostsList/PostsList";
 const Home = () => {
 
     const [posts, setPosts] = useState(null);
-
-    useEffect(() => {
-        fetch(" http://localhost:8000/posts").then(response => response.json()).then(data => setPosts(data));
-    }, []);
+    const [isPending, setIsPending] = useState(true);
 
     const handleDelete = (postId) => {
         setPosts(posts.filter(post => post.id !== postId));
     }
 
+    useEffect(() => {
+        fetch(" http://localhost:8000/posts")
+            .then(response => response.json())
+            .then(data => {
+                setPosts(data);
+                setIsPending(false);
+            });
+    }, []);
+
     return (
-        <>
-            {posts &&
-            <div className="container home">
-                <PostsList title="All Posts" posts={posts} handleDelete={handleDelete} />
+        <div className="container home">
+            {isPending && <p>Loading...</p>}
+            {posts && <>
+                <PostsList title="All Posts" posts={posts} handleDelete={handleDelete}/>
                 <PostsList
                     title="Mario's Posts"
                     posts={posts.filter(post => post.author === "Mario")}
                     handleDelete={handleDelete}
                 />
-            </div>}
-        </>
+            </>}
+        </div>
     );
 }
 
