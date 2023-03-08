92a897045a70a8c36ef2a6f8d39548a19b12af22
ryhhill1998 <ryhhill1998@outlook.com>
2023-03-08 11:55:15 +0000

    Improved css appearance of form and added functionality to update view on change

--- src/routes/Create/Create.jsx
+++ src/routes/Create/Create.jsx
@@ -1,26 +1,53 @@
 import "./Create.css";
 import {faPlus} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {useState} from "react";
+
+const defaultFormFields = {
+    title: "",
+    content: "",
+    author: "Mario"
+};
 
 const Create = () => {
+
+    const [formFields, setFormFields] = useState(defaultFormFields);
+
+    const {title, content, author} = formFields;
+
+    const handleFieldChange = ({target}) => {
+        const {name, value} = target;
+
+        setFormFields(formFields => {
+            const updatedFields = {...formFields};
+            updatedFields[name] = value;
+            return updatedFields;
+        });
+    };
+
+    const handleFormSubmitted = (e) => {
+        e.preventDefault();
+        console.log(title, content, author);
+    };
+
     return (
         <div className="create-post-container container">
             <h2>Create New Post</h2>
 
-            <form className="create-post-form">
+            <form className="create-post-form" onSubmit={handleFormSubmitted}>
                 <label>
                     Title
-                    <input />
+                    <input type="text" name="title" value={title} onChange={handleFieldChange} required />
                 </label>
 
                 <label>
                     Content
-                    <textarea />
+                    <textarea name="content" rows="8" value={content} onChange={handleFieldChange} required />
                 </label>
 
                 <label>
                     Author
-                    <select>
+                    <select name="author" value={author} onChange={handleFieldChange} required>
                         <option>Mario</option>
                         <option>Luigi</option>
                         <option>Yoshi</option>
